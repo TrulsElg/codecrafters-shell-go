cf10a4fe6135a740102ce8a0ffc6485b89c5fc1b
TrulsElg <trulseb@gmail.com>
2024-12-05 12:40:13 +0100

    exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,6 +22,13 @@ func main() {
 			os.Exit(1)
 		}
 		command = strings.TrimSuffix(command, "\n")
+		words := strings.Fields(command)
+		if len(words) > 1 {
+			if exit_code, err := strconv.ParseInt(words[1], 10, 64); err == nil && words[0] == "exit" {
+				os.Exit(int(exit_code))
+			}
+		}
+		
 		fmt.Fprintln(os.Stdout, command+": command not found")
 	}
 
