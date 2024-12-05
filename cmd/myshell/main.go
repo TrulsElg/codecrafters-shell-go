273c36dc97afdc1b8c0dd13846522ba68256490e
TrulsElg <trulseb@gmail.com>
2024-12-05 12:53:45 +0100

    echo command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -16,20 +16,28 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		command = strings.TrimSuffix(command, "\n")
-		words := strings.Fields(command)
-		if len(words) > 1 {
-			if exit_code, err := strconv.ParseInt(words[1], 10, 64); err == nil && words[0] == "exit" {
-				os.Exit(int(exit_code))
+
+		input = strings.TrimSuffix(input, "\n")
+		words := strings.Fields(input)
+		command := words[0]
+
+		switch command {
+		case "exit":
+			if exitCode, err := strconv.ParseInt(words[1], 10, 64); err == nil && words[0] == "exit" {
+				os.Exit(int(exitCode))
+			} else {
+				os.Exit(0)
 			}
+		case "echo":
+			fmt.Println(strings.Join(words[1:], " "))
+		default:
+			fmt.Fprintln(os.Stdout, command+": command not found")
 		}
-		
-		fmt.Fprintln(os.Stdout, command+": command not found")
 	}
 
 }
