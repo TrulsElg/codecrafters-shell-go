9c0f6db709fbcc5485dad9fa68f9d1e520713b46
TrulsElg <trulseb@gmail.com>
2024-12-08 20:49:53 +0100

    type: executable files

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"slices"
@@ -11,6 +12,34 @@ import (
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
+
+func checkPathForProgram(path string, command string) (commandPath string, err error) {
+	directories := strings.Split(path, ":")
+
+	for _, dir := range directories {
+		//fmt.Println(dir)
+
+		f, err := os.Open(dir)
+		if err != nil {
+			continue
+			//fmt.Println(err)
+		}
+
+		files, err := f.Readdirnames(0)
+		f.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, file := range files {
+			if file == command {
+				return dir + "/" + command, nil
+			}
+		}
+
+	}
+
+	return "", errors.New(command + ": not found")
+}
 
 func main() {
 	for {
@@ -25,6 +54,10 @@ func main() {
 
 		input = strings.TrimSuffix(input, "\n")
 		words := strings.Fields(input)
+
+		if len(words) == 0 {
+			continue
+		}
 		command := words[0]
 
 		switch command {
@@ -49,6 +82,8 @@ func main() {
 				command := words[1]
 				if slices.Contains(builtinCommands, command) {
 					fmt.Fprintln(os.Stdout, command+" is a shell builtin")
+				} else if commandPath, err := checkPathForProgram(os.Getenv("PATH"), command); err == nil {
+					fmt.Fprintln(os.Stdout, command+" is "+commandPath)
 				} else {
 					fmt.Fprintln(os.Stdout, command+": not found")
 				}
